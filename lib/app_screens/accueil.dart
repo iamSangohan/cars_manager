4e4dc385f6e842476e71e5cd36f2d6b79bfbb4a7
iamSangohan <mensahluc81@gmail.com>
2023-11-05 11:59:38 +0000

    #feat : Add user and vehicle information with buttons for incidents and fuel supply

--- lib/app_screens/accueil.dart
+++ lib/app_screens/accueil.dart
@@ -9,8 +9,74 @@ class AccueilScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Accueil'),
       ),
-      body: const Center(
-        child: Text( 'Accueil'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            // Ligne avec les informations de l'utilisateur
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text('NOM : ', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+              Text("MENSAH Luc", style: TextStyle(fontSize: 18)),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Row(
+            // Ligne avec les informations sur le vehicule de l'utilisateur
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text('Vehicule No : ', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+              Text("AB-100-AB", style: TextStyle(fontSize: 18)),
+            ],
+          ),
+          const SizedBox(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Deux boutons de meme taille au centre avec une image dans chaque bouton pour Incidents et Approvisionnements
+              Expanded(
+                child: SizedBox(
+                  height: 150,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Navigate to second route when tapped.
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.car_repair, size: 40),
+                          Text('Incidents', style: TextStyle(fontSize: 16)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: 150,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Navigate to second route when tapped.
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.local_gas_station, size: 40),
+                          Text('Approvisionnements', style: TextStyle(fontSize: 16)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ]
       ),
     );
   }
