fb3fdc769d7d8f8524ed6708ceb0111153dd36cb
iamSangohan <mensahluc81@gmail.com>
2023-11-05 11:58:16 +0000

    #feat : Update Splash to redirect to LoginScreen

--- lib/app_screens/splashScreen.dart
+++ lib/app_screens/splashScreen.dart
@@ -1,6 +1,6 @@
+import 'package:cars_manager/app_screens/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:cars_manager/app_screens/accueil.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -18,7 +18,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
 
     Future.delayed(const Duration(seconds: 3), () {
       Navigator.of(context).pushReplacement(MaterialPageRoute(
-        builder: (_) => const AccueilScreen(),
+        builder: (_) => const LoginScreen(),
       ));
     });
   }
