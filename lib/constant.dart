ff6e5dab559c80d2b3807690375122228fcbd94a
iamSangohan <mensahluc81@gmail.com>
2023-11-08 16:51:08 +0000

    #feat : Add global variables

---
+++ lib/constant.dart
@@ -0,0 +1,7 @@
+import 'package:cars_manager/models/user.dart';// Importez votre classe User
+
+// Variable globale pour stocker l'utilisateur connecté
+User? currentUser;
+
+// Variable globale pour stocker le token d'authentification
+String? access_token;
