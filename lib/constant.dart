af698df207195dbb66e70f3cf1a3d6db4e410a93
iamSangohan <mensahluc81@gmail.com>
2023-11-09 09:23:58 +0000

    #refactor : Remove unused constant variable

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,6 +2,3 @@ import 'package:cars_manager/models/user.dart';// Importez votre classe User
 
 // Variable globale pour stocker l'utilisateur connecté
 User? currentUser;
-
-// Variable globale pour stocker le token d'authentification
-String? access_token;
